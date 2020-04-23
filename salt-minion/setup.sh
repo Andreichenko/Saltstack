f1d65850ce6bed883b3e0d26ef406e0099199377
frei <alexfrei.andreichenko@gmail.com>
2020-04-24 02:42:11 +0300

    this script does on the Minion instances is indicate the host salt-master is the master this minion takes its instructions from

--- salt-minion/setup.sh
+++ salt-minion/setup.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+salt_minion_check () {
+	if [ ! -f /var/log/salt/minion ]; then
+  	  echo "File not found!" && \
+          touch /var/log/salt/minion && \
+	  salt_minion_check
+	else
+	  tail -f /var/log/salt/minion
+	fi
+}
